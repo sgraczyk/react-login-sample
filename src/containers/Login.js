2eae0ae94d75d3254136bc61efb74efae6e20d81
Szymon Graczyk <sz.graczyk@gmail.com>
2018-01-30 19:14:41 +0100

    Initial phase of login page. Very simple html version, yet to divide into components, set state etc. Added transform-class-properties to avoid necessity of binding this to handlers in constructor.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    username: '',
+    password: ''
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // set state
+    // dispatch action
   }
 
   render() {
-    const { loggingIn } = this.props;
-    const { username, password, submitted } = this.state;
     return (
       <div className="login-container">
         <h2>Login</h2>
         <form onSubmit={this.handleSubmit}>
           <fieldset>
-            <label for="email">email</label>
+            <label htmlFor="email">email</label>
             <input type="text" name="email" id="email" />
-            <label for="password">password</label>
+            <label htmlFor="password">password</label>
             <input type="password" name="password" id="password" />
-            <label for="remember">Remember me</label>
+            <label htmlFor="remember">Remember me</label>
             <input type="checkbox" name="remember" id="remember" />
             <input type="submit" value="login" />
           </fieldset>
