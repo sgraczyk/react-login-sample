02e311a70cb80a2a12abb8371ab63e637e2e590a
Szymon Graczyk <sz.graczyk@gmail.com>
2018-01-30 20:47:40 +0100

    Handle basic change events. Prepare for components division.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,7 +4,9 @@ import { injectIntl, intlShape } from 'react-intl';
 class Login extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    rememberMe: false,
+    isSubmitted: false
   };
 
   handleSubmit = (e) => {
@@ -13,20 +15,59 @@ class Login extends Component {
     // dispatch action
   }
 
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleChecked = (e) => {
+    const { name, checked } = e.target;
+    this.setState({
+      [name]: checked
+    });
+  }
+
   render() {
     const { intl } = this.props;
+    const { email, password, rememberMe } = this.state;
     return (
       <div className="login-container">
         <h2>{intl.formatMessage({ id: 'login.header' })}</h2>
         <form onSubmit={this.handleSubmit}>
           <fieldset>
-            <label htmlFor="email">{intl.formatMessage({ id: 'login.email' })}</label>
-            <input type="text" name="email" id="email" />
-            <label htmlFor="password">{intl.formatMessage({ id: 'login.password' })}</label>
-            <input type="password" name="password" id="password" />
-            <label htmlFor="remember">{intl.formatMessage({ id: 'login.rememberMe' })}</label>
-            <input type="checkbox" name="remember" id="remember" />
-            <input type="submit" value="login" />
+            <label htmlFor="email">
+              {intl.formatMessage({ id: 'login.email' })}
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="password">
+              {intl.formatMessage({ id: 'login.password' })}
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="remember">
+              {intl.formatMessage({ id: 'login.rememberMe' })}
+            </label>
+            <input
+              type="checkbox"
+              name="rememberMe"
+              id="remember"
+              checked={rememberMe}
+              onChange={this.handleChecked}
+            />
+            <input type="submit" />
           </fieldset>
         </form>
       </div>
